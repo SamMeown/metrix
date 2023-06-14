f1a2c9d85bc12a7beb9e6292bc17ad2c7a193d64
Sam <sammeown@gmail.com>
2023-06-14 11:38:57 +0300

    Increment2: server - added tests for update handler, fixed handler logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -42,69 +42,72 @@ func (m MemStorage) SetCounter(name string, value int64) error {
 
 var storage MetricsStorage = MemStorage{make(map[string]any)}
 
-func handleUpdate(res http.ResponseWriter, req *http.Request) {
-	if req.Method != http.MethodPost {
-		http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+func handleUpdate(storage MetricsStorage) func(http.ResponseWriter, *http.Request) {
+	return func(res http.ResponseWriter, req *http.Request) {
+		if req.Method != http.MethodPost {
+			http.Error(res, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
+
+		res.Header().Set("Content-Type", "text/plain; charset=utf-8")
+
+		path := req.URL.Path
+		fmt.Printf("Path: %s\n", path)
+
+		var metricsType, metricsName string
+		var metricsValue any
+
+		components := strings.Split(strings.Trim(path, "/"), "/")[1:]
+		//fmt.Printf("Components: %v", components)
+		if len(components) < 1 || len(components) > 3 {
+			http.Error(res, "Wrong number of data components", http.StatusBadRequest)
+			return
+		}
+
+		metricsType = components[0]
+		if metricsType != MetricsTypeGauge &&
+			metricsType != MetricsTypeCounter {
+			http.Error(res, "Wrong metrics type", http.StatusBadRequest)
+			return
+		}
+
+		if len(components) < 2 {
+			http.Error(res, "No metrics name", http.StatusNotFound)
+			return
+		}
+
+		metricsName = components[1]
+
+		if len(components) < 3 {
+			http.Error(res, "No metrics value", http.StatusBadRequest)
+			return
+		}
+
+		var convErr error
+		if metricsType == MetricsTypeGauge {
+			metricsValue, convErr = strconv.ParseFloat(components[2], 64)
+		} else {
+			metricsValue, convErr = strconv.ParseInt(components[2], 10, 64)
+		}
+		if convErr != nil {
+			http.Error(res, "Can not parse metrics value", http.StatusBadRequest)
+			return
+		}
+
+		//fmt.Printf("type: %s, name: %s, value: %v", metricsType, metricsName, metricsValue)
+		if metricsType == MetricsTypeGauge {
+			storage.SetGauge(metricsName, metricsValue.(float64))
+		} else {
+			storage.SetCounter(metricsName, metricsValue.(int64))
+		}
+
+		res.WriteHeader(http.StatusOK)
 	}
-
-	res.Header().Set("Content-Type", "text/plain; charset=utf-8")
-
-	path := req.URL.Path
-	fmt.Printf("Path: %s\n", path)
-
-	var metricsType, metricsName string
-	var metricsValue any
-
-	components := strings.Split(strings.Trim(path, "/"), "/")[1:]
-	//fmt.Printf("Components: %v", components)
-	if len(components) < 1 || len(components) > 3 {
-		http.Error(res, "Wrong number of data components", http.StatusBadRequest)
-		return
-	}
-
-	metricsType = components[0]
-	if metricsType != MetricsTypeGauge &&
-		metricsType != MetricsTypeCounter {
-		http.Error(res, "Wrong metrics type", http.StatusBadRequest)
-		return
-	}
-
-	if len(components) < 2 {
-		http.Error(res, "No metrics name", http.StatusNotFound)
-		return
-	}
-
-	metricsName = components[1]
-
-	if len(components) < 3 {
-		http.Error(res, "No metrics value", http.StatusBadRequest)
-	}
-
-	var convErr error
-	if metricsType == MetricsTypeGauge {
-		metricsValue, convErr = strconv.ParseFloat(components[2], 64)
-	} else {
-		metricsValue, convErr = strconv.ParseInt(components[2], 10, 64)
-	}
-	if convErr != nil {
-		http.Error(res, "Can not parse metrics value", http.StatusBadRequest)
-		return
-	}
-
-	//fmt.Printf("type: %s, name: %s, value: %v", metricsType, metricsName, metricsValue)
-	if metricsType == MetricsTypeGauge {
-		storage.SetGauge(metricsName, metricsValue.(float64))
-	} else {
-		storage.SetCounter(metricsName, metricsValue.(int64))
-	}
-
-	res.WriteHeader(http.StatusOK)
 }
 
 func main() {
 	mux := http.NewServeMux()
-	mux.HandleFunc("/update/", handleUpdate)
+	mux.HandleFunc("/update/", handleUpdate(storage))
 
 	err := http.ListenAndServe(`:8080`, mux)
 	if err != nil {
---
+++ cmd/server/main_test.go
@@ -0,0 +1,110 @@
+package main
+
+import (
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestHandleUpdate(t *testing.T) {
+	type want struct {
+		statusCode  int
+		contentType string
+	}
+	tests := []struct {
+		name          string
+		requestMethod string
+		requestPath   string
+		want          want
+	}{
+		{
+			name:          "test no metrics type",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test wrong metrics type",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/gaga",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test no metrics name",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/gauge",
+			want: want{
+				statusCode:  http.StatusNotFound,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test no metrics value",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/gauge/a",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test wrong metrics value",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/gauge/a/notanumber",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test right gauge request",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/gauge/a/11.35",
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test right counter request",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/counter/a/11",
+			want: want{
+				statusCode:  http.StatusOK,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+		{
+			name:          "test counter request with wrong (float) value",
+			requestMethod: http.MethodPost,
+			requestPath:   "/update/counter/a/11.35",
+			want: want{
+				statusCode:  http.StatusBadRequest,
+				contentType: "text/plain; charset=utf-8",
+			},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			storage := MemStorage{make(map[string]any)}
+			req := httptest.NewRequest(tt.requestMethod, tt.requestPath, nil)
+			recorder := httptest.NewRecorder()
+			handler := http.HandlerFunc(handleUpdate(storage))
+
+			handler(recorder, req)
+
+			result := recorder.Result()
+
+			assert.Equal(t, result.StatusCode, tt.want.statusCode)
+			assert.Equal(t, result.Header.Get("Content-Type"), tt.want.contentType)
+		})
+	}
+}
