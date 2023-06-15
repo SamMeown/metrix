b40691bf18a5435be7a4a8d61edb8a7d21c3422a
Sam <sammeown@gmail.com>
2023-06-15 03:10:40 +0300

    Increment5: server - handle environment variables

--- cmd/server/flags.go
+++ cmd/server/flags.go
@@ -1,10 +1,17 @@
 package main
 
-import "flag"
+import (
+	"flag"
+	"os"
+)
 
 var flagAddress string
 
 func parseFlags() {
 	flag.StringVar(&flagAddress, "a", ":8080", "server address and port")
 	flag.Parse()
+
+	if envAddress, ok := os.LookupEnv("ADDRESS"); ok {
+		flagAddress = envAddress
+	}
 }
