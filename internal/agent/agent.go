a3f215b9611e6ec2d8bb64e94cb21889cccf197c
Sam <sammeown@gmail.com>
2023-09-03 11:25:38 +0300

    Increment15: goroutines: agent - initially switched to separate goroutines for refreshing and reporting metrics

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -1,6 +1,7 @@
 package agent
 
 import (
+	"sync"
 	"time"
 
 	"github.com/SamMeown/metrix/internal/agent/client"
@@ -9,28 +10,55 @@ import (
 	"github.com/SamMeown/metrix/internal/logger"
 )
 
+var wg sync.WaitGroup
+var m sync.Mutex
+
+func startRefreshMetricsLoop(conf config.Config, collector metrics.Collector) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(time.Duration(conf.PollInterval) * time.Second)
+		for {
+			m.Lock()
+			logger.Log.Infoln("Refreshing metrics...")
+			collector.CollectMetrics()
+			m.Unlock()
+			select {
+			case <-ticker.C:
+				//continue
+			}
+		}
+	}()
+}
+
+func startReportMetricsLoop(conf config.Config, collector metrics.Collector, client *client.MetricsClient) {
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+		ticker := time.NewTicker(time.Duration(conf.ReportInterval) * time.Second)
+		for {
+			m.Lock()
+			logger.Log.Infoln("Reporting metrics...")
+			client.ReportAllMetrics(collector.Collection())
+			collector.ResetCollectsCount()
+			m.Unlock()
+			select {
+			case <-ticker.C:
+				//continue
+			}
+		}
+	}()
+}
+
 func Start(conf config.Config, collector metrics.Collector, client *client.MetricsClient) {
-	err := logger.Initialize("info")
+	err := logger.Initialize("debug")
 	if err != nil {
 		panic(err)
 	}
 	defer logger.Log.Sync()
 
-	var secondsElapsed int64
+	startRefreshMetricsLoop(conf, collector)
+	startReportMetricsLoop(conf, collector, client)
 
-	for {
-		if secondsElapsed%int64(conf.PollInterval) == 0 {
-			logger.Log.Infoln("Refreshing metrics...")
-			collector.CollectMetrics()
-		}
-
-		if secondsElapsed%int64(conf.ReportInterval) == 0 {
-			logger.Log.Infoln("Reporting metrics...")
-			client.ReportAllMetrics(collector.Collection())
-			collector.ResetCollectsCount()
-		}
-
-		secondsElapsed++
-		time.Sleep(1 * time.Second)
-	}
+	wg.Wait()
 }
