ef0e43f5f1ff1e35e3425a4c6a1c24bed7984fff
Sam <sammeown@gmail.com>
2023-09-03 14:15:55 +0300

    Increment15: goroutines: agent - added psutil metrics for memory and cpu utilization

--- internal/agent/metrics/metrics.go
+++ internal/agent/metrics/metrics.go
@@ -2,6 +2,9 @@ package metrics
 
 import (
 	"context"
+	"github.com/SamMeown/metrix/internal/logger"
+	"github.com/shirou/gopsutil/v3/cpu"
+	"github.com/shirou/gopsutil/v3/mem"
 	"math/rand"
 	"reflect"
 	"runtime"
@@ -70,9 +73,36 @@ func (mg *metricsCollector) CollectMetrics() {
 
 	ctx := context.Background()
 	mg.collectMemstatMetrics(ctx)
+	mg.collectPsutilMetrics(ctx)
 
 	mg.collection.SetGauge(ctx, "RandomValue", float64(rand.Int()))
 	mg.collection.SetCounter(ctx, "PollCount", mg.collectsCount)
+}
+
+func (mg *metricsCollector) collectPsutilMemMetrics(ctx context.Context) {
+	memStat, err := mem.VirtualMemory()
+	if err != nil {
+		logger.Log.Errorf("Failed to get mem info: %s", err)
+		return
+	}
+
+	mg.collection.SetGauge(ctx, "TotalMemory", gauge(memStat.Total))
+	mg.collection.SetGauge(ctx, "FreeMemory", gauge(memStat.Available))
+}
+
+func (mg *metricsCollector) collectPsutilCpuMetrics(ctx context.Context) {
+	cpuPercent, err := cpu.Percent(0, false)
+	if err != nil {
+		logger.Log.Errorf("Failed to get cpu percentage: %s", err)
+		return
+	}
+
+	mg.collection.SetGauge(ctx, "CPUutilization1", cpuPercent[0])
+}
+
+func (mg *metricsCollector) collectPsutilMetrics(ctx context.Context) {
+	mg.collectPsutilMemMetrics(ctx)
+	mg.collectPsutilCpuMetrics(ctx)
 }
 
 func (mg *metricsCollector) collectMemstatMetrics(ctx context.Context) {
